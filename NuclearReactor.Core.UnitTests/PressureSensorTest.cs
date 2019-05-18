a7c7647d01c99cb5f77f08e3ef52eea0a109fed4
abragsta <anders@bragstad.com>
2019-05-18 19:13:56 +0200

    Test to see that PressureSensor returns value from Pressure Container.

---
+++ NuclearReactor.Core.UnitTests/PressureSensorTest.cs
@@ -0,0 +1,24 @@
+﻿using NSubstitute;
+using NuclearReactor.Core.Contracts;
+using Xunit;
+
+namespace NuclearReactor.Core.UnitTests
+{
+    public class PressureSensorTest
+    {
+        [Fact]
+        public void GetValue_Void_ReturnsPressureValue()
+        {
+            var pressureContainer = Substitute.For<IPressureContainer>();
+            var pressureSensor = new PressureSensor(pressureContainer);
+
+            var expectedValue = 0.6f;
+
+            pressureContainer.Pressure.Returns(expectedValue);
+
+            var actualValue = pressureSensor.GetValue();
+
+            Assert.Equal(expectedValue, actualValue);
+        }
+    }
+}
