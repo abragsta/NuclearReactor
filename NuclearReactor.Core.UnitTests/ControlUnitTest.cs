cb63327af54d19e769d9f2e77e82f222021e066a
abragsta <anders@bragstad.com>
2019-05-18 17:07:20 +0200

    Added failing for to check if valve is closed over 75%.

---
+++ NuclearReactor.Core.UnitTests/ControlUnitTest.cs
@@ -0,0 +1,31 @@
+﻿using NSubstitute;
+using NSubstitute.ReceivedExtensions;
+using NuclearReactor.Core.Contracts;
+using Xunit;
+
+namespace NuclearReactor.Core.UnitTests
+{
+    public class ControlUnitTest
+    {
+        private readonly ControlUnit _controlUnit;
+        private readonly IPressureSensor _pressureSensor;
+        private readonly IValveControl _valveControl;
+
+        public ControlUnitTest()
+        {
+            _pressureSensor = Substitute.For<IPressureSensor>();
+            _valveControl = Substitute.For<IValveControl>();
+            _controlUnit = new ControlUnit(_valveControl, _pressureSensor);
+        }
+
+        [Fact]
+        public void InitiateControl_PressureIsAbove75_ValveIsClosed()
+        {
+            _pressureSensor.GetValue().Returns(76);
+
+            _controlUnit.InitiateControl();
+
+            _valveControl.Received().Close();
+        }
+    }
+}
