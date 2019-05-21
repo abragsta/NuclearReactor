210bf325452927921f749baf07d2f464587037e2
abragsta <anders@bragstad.com>
2019-05-21 19:36:21 +0200

    Added progress bar to visualize pressure.

--- nuclear-reactor-web/src/PressureReading/PressureReading.js
+++ nuclear-reactor-web/src/PressureReading/PressureReading.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import * as signalR from '@aspnet/signalr';
+import { Line } from 'rc-progress';
 
 class PressureReading extends Component {
     state = {
@@ -13,7 +14,7 @@ class PressureReading extends Component {
 
         connection.on("pressureUpdate", data => {
             console.log(data);
-            this.setState({pressure: data})
+            this.setState({ pressure: (data * 100).toFixed(0) })
         });
 
         connection.start();
@@ -21,7 +22,10 @@ class PressureReading extends Component {
 
     render() {
         return (
-            <div>Nuclear reactor pressure: {(this.state.pressure * 100).toFixed(0)}%</div>
+            <div>
+                <div>Nuclear reactor pressure: {this.state.pressure}%</div>
+                <Line percent={this.state.pressure} strokeWidth="4" strokeColor="#4286f4" />
+            </div>
         );
     }
 }
